200b42b0683216c428140d7864aa9683dfee43eb
runningship <runningship@gmail.com>
2014-12-10 18:26:25 +0800

    more

--- src/main/java/com/youwei/leshi/StartUpListener.java
+++ src/main/java/com/youwei/leshi/StartUpListener.java
@@ -11,6 +11,7 @@ import org.bc.sdak.SessionFactoryBuilder;
 import org.hibernate.cfg.AvailableSettings;
 
 import com.youwei.SQL2008Dialect;
+import com.youwei.leshi.cache.ConfigCache;
 import com.youwei.web.ModuleManager;
 import com.youwei.web.PublicFieldSupportingELResolver;
 
@@ -50,8 +51,8 @@ public class StartUpListener implements ServletContextListener{
 		settings.put(AvailableSettings.CACHE_REGION_FACTORY, "org.hibernate.cache.ehcache.EhCacheRegionFactory");
 		settings.put(AvailableSettings.USE_SECOND_LEVEL_CACHE, "true");
 		
-		settings.put(AvailableSettings.PROXOOL_XML, "proxool.xml");//相对目录为classes
-//		settings.put(AvailableSettings.PROXOOL_XML, ConfigCache.get("proxool_xml" ,"proxool.xml"));//相对目录为classes
+//		settings.put(AvailableSettings.PROXOOL_XML, "proxool.xml");//相对目录为classes
+		settings.put(AvailableSettings.PROXOOL_XML, ConfigCache.get("proxool_xml" ,"proxool.xml"));//相对目录为classes
 		settings.put(AvailableSettings.PROXOOL_EXISTING_POOL, "false");
 		settings.put(AvailableSettings.PROXOOL_POOL_ALIAS, "mySqlProxool");
 		
